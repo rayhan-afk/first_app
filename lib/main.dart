61944f2a4eae3c26bcc387fbe2683c60865a117a
Rayhan Abduhuda <rayrayhan920@gmail.com>
2023-10-17 11:42:19 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Selamat datang, Ray'),
     );
   }
 }
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Jangan klik lagi, Ray :',
+              'Jangan klik lagi yaa Ray :',
             ),
             Text(
               '$_counter',
