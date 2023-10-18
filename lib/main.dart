62c238853ddaf3164ddeb00e1bde9db94741462a
Rayhan Abduhuda <rayrayhan920@gmail.com>
2023-10-18 20:38:57 +0700

    class

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-     MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [ 
-                Color.fromARGB(255, 99, 37, 206),
-                Color.fromARGB(255, 7, 183, 241),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text('Hello World!'),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 99, 37, 206),
+            Color.fromARGB(255, 7, 183, 241),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Selamat Datang!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
 }
