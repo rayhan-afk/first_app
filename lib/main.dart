6fff788757c7f99b0aea9ed4c09e04e21517c997
Rayhan Abduhuda <rayrayhan920@gmail.com>
2023-10-18 15:29:00 +0700

    color

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+       backgroundColor: Color.fromARGB(255, 19, 19, 235), 
         body: Center(
           child: Text('Hello World!'),
         ),
