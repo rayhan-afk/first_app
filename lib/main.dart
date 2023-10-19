eab427148868213387bd63902cf09c2dfa7caa52
Rayhan Abduhuda <rayrayhan920@gmail.com>
2023-10-19 20:54:59 +0700

    dice roller

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: GradientContainer(
-          const Color.fromARGB(255, 52, 12, 229),
-          const Color.fromARGB(255, 87, 59, 214)
+          Color.fromARGB(255, 52, 12, 229),
+          Color.fromARGB(255, 87, 59, 214)
         ),
       ),
     ),
